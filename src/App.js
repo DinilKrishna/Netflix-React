6c6acaad8ce7f9f25a11a6e85d8993ee7f9e8cd3
dinil <dinilkrishna46@gmail.com>
2024-09-10 21:26:58 +0530

    API added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   return (
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost />
+      <RowPost title='Exciting Films'/>
+      <RowPost title='Action & Adventure Films'/>
+      <RowPost title='Blockbuster Films'/>
     </div>
   );
 }
---
+++ src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseURL = 'https://api.themoviedb.org/3'
+export const APIKey = '04ba46e3c7a17164082261ca75a83446'
+export const ImageURL = 'https://image.tmdb.org/t/p/w500'
