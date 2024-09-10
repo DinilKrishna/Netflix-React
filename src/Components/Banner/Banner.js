ddec708f7276020a5873ac74bd226c1f6cc7d298
dinil <dinilkrishna46@gmail.com>
2024-09-10 17:31:57 +0530

    Netflix HTML structure

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content' >
+            <h1 className='title'>Movies</h1>
+            {/* <div className='banner_buttons' >
+                <button className='button' >Play</button>
+                <button className='button' >My list</button>
+            </div> */}
+            <h1 className='description'>Movies move us like nothing else can, whether they’re scary, funny, dramatic, romantic or anywhere in-between. So many titles, so much to experience.</h1>
+        </div>
+        <div className="fade_bottom"></div>
+    </div>
+  )
+}
+
+export default Banner
